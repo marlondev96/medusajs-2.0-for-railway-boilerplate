c8f493626f92a063bfa1a8e8ced580dc0de08397
marlondev96 <135303248+marlondev96@users.noreply.github.com>
2025-03-31 23:21:08 -0400

    name

--- storefront/src/app/[countryCode]/(main)/collections/[handle]/page.tsx
+++ storefront/src/app/[countryCode]/(main)/collections/[handle]/page.tsx
@@ -59,7 +59,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   }
 
   const metadata = {
-    title: `${collection.title} | Medusa Store`,
+    title: `${collection.title} | Eli&Mar Shop`,
     description: `${collection.title} collection`,
   } as Metadata
 
--- storefront/src/modules/layout/templates/nav/index.tsx
+++ storefront/src/modules/layout/templates/nav/index.tsx
@@ -25,7 +25,7 @@ export default async function Nav() {
               className="txt-compact-xlarge-plus hover:text-ui-fg-base uppercase"
               data-testid="nav-store-link"
             >
-              Medusa Store
+              Eli&Mar Shop
             </LocalizedClientLink>
           </div>
 
@@ -38,7 +38,7 @@ export default async function Nav() {
                   scroll={false}
                   data-testid="nav-search-link"
                 >
-                  Search
+                  Busqueda
                 </LocalizedClientLink>
               )}
               <LocalizedClientLink
@@ -46,7 +46,7 @@ export default async function Nav() {
                 href="/account"
                 data-testid="nav-account-link"
               >
-                Account
+                Cuenta
               </LocalizedClientLink>
             </div>
             <Suspense
@@ -56,7 +56,7 @@ export default async function Nav() {
                   href="/cart"
                   data-testid="nav-cart-link"
                 >
-                  Cart (0)
+                  Carta (0)
                 </LocalizedClientLink>
               }
             >
